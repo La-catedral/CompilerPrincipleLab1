1cfabb51ec75a868124798bf98a1ab52287efe87
La-catedral <53202870+La-catedral@users.noreply.github.com>
2022-04-17 22:14:10 +0800

    public commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+OS：Mac OS 11.2
+Library：Python 3.8
+
+在当前目录下运行Lexical.py文件，并以需要编译的c文件作为第一个参数，以输出的文件名作为第二个参数
+默认输出到lex_output.txt文件中。
+例如：python Lexical.py lexical_input.c
+
---
+++ src/lexical_input.c
@@ -0,0 +1,42 @@
+char function () {
+	while(val < 10)
+	{val++;}
+}
+int main()
+{
+
+    struct Stru {
+    int token;
+    int Bvalue;
+    };
+	int a;
+	float _b;
+	int fad_213;
+	int Arr[5];
+	float e;
+
+	c = 1e-8;
+	e = .
+	f = 0x
+	char *charArray = "Hello World!";
+	char anotherArray[] = "Hello World!;"
+
+	if(c > 0) {
+		a = 023;
+		d = 0xa2;
+	}
+	else {
+		b = 1.11 * 8.9;
+	}
+	while(1) {
+		b = 10.44;
+		e = 990.45;
+		c = 90;
+	}
+	c = 8 & 7;
+	Arr[2] = 5
+	e = Arr[2];
+	function();
+	@
+	/*this is an annotation!
+}
